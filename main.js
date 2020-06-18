7df87cd9f09672581a94398c5acb12a88c38cae2
hiromiogawa <44115109+hiromiogawa@users.noreply.github.com>
2020-06-18 23:19:04 +0900

    tab01

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-var app = new Vue({
+new Vue({
   el: "#example",
   data: {
-    count: 0
-  },
-  computed: {
-    date: function () {
-      return new Date().toLocaleString();
-    }
-  },
-  methods: {
-    countUp: function () {
-      this.count += 1;
-    },
-    getDate: function () {
-      return new Date().toLocaleString();
-    }
+    activeTab: "tabs-1"
   }
 });
